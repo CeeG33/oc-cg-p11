0fe9582918e8633d301c01cd63988650c16d2cff
Ciran GÜRBÜZ <ciran.gurbuz@gmail.com>
2023-09-14 15:40:21 +0200

    Bug résolu et tests réussis

--- tests/conftest.py
+++ tests/conftest.py
@@ -41,13 +41,18 @@ def monkey_competitions(monkeypatch):
     competitions = [
         {
             "name": "Spring Festival",
-            "date": "2020-03-27 10:00:00",
+            "date": "2030-03-27 10:00:00",
             "numberOfPlaces": "25"
         },
         {
             "name": "Fall Classic",
             "date": "2020-10-22 13:30:00",
             "numberOfPlaces": "13"
+        },
+        {
+            "name": "Future Competition",
+            "date": "2030-10-22 13:30:00",
+            "numberOfPlaces": "15"
         }
     ]
     
